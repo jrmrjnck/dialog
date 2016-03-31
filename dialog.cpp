23b89f93f724ff82a2433c3e9fcaa29c819d46fc
Jonathan Doman <jonathan.doman@gmail.com>
2016-03-31 00:16:22 -0700

    Add persistence so it can be used outside of cron.
    Use Qt command line parser and add period option so that you can
    effectively set an arbitrary duty cycle for showing the dialog box.

--- dialog.cpp
+++ dialog.cpp
@@ -1,26 +1,111 @@
+/*
+ * This is free and unencumbered software released into the public domain.
+ */
+
 #include <QMessageBox>
 #include <QApplication>
 #include <QTimer>
+#include <QCommandLineParser>
+
+static
+void addOptions( QCommandLineParser& argparser )
+{
+   const QList<QStringList> options = {
+      {"message", "text", "Message text.", "Hello, world."},
+      {"window-title", "text", "Window title.", "Timer"},
+      {"period", "sec", "Pop-up period. (0 = disable)", "0"},
+      {"timeout", "sec", "Time to show window.", QString::number(30)}
+      };
+
+   for(auto& option : options)
+   {
+      argparser.addOption(
+         QCommandLineOption(
+            {QString(option[0][0]), option[0]},
+            QString("%1 (Default: %2)").arg(option[2]).arg(option[3]),
+            option[1],
+            option[3]
+            )
+         );
+   }
+}
+
+class DialogBox : public QMessageBox
+{
+private:
+   int _period;
+   int _timeout;
+
+public:
+   DialogBox(
+      const QString& windowTitle,
+      const QString& message,
+      int period,
+      int timeout
+      )
+    : _period(period),
+      _timeout(timeout)
+   {
+      setIcon( QMessageBox::Information );
+      setWindowTitle( windowTitle );
+      setText( message );
+      setWindowFlags( Qt::WindowStaysOnTopHint );
+
+      startTimeout();
+   }
+
+   void startTimeout()
+   {
+      show();
+      QTimer::singleShot( 1000 * _timeout, this, &DialogBox::endTimeout );
+   }
+
+   void endTimeout()
+   {
+      if( _period > 0)
+      {
+         hide();
+         QTimer::singleShot( 1000 * (_period - _timeout), this, &DialogBox::startTimeout );
+      }
+      else
+      {
+         close();
+      }
+   }
+
+   void done( int r )
+   {
+      if( _period > 0 )
+      {
+         hide();
+      }
+      else
+      {
+         QMessageBox::done( r );
+      }
+   }
+};
 
 int main( int argc, char* argv[] )
 {
-  QApplication app( argc, argv );
+   QApplication app( argc, argv );
+   QApplication::setApplicationName( "dialog" );
 
-  auto args = app.arguments();
-  auto text = args.value( 1, "No text provided" );
-  auto title = args.value( 2, "Timer" );
-  auto time = args.value( 3, "30" ).toInt();
+   QCommandLineParser argparse;
+   argparse.setApplicationDescription( "A simple timed dialog box." );
+   argparse.addHelpOption();
+   addOptions( argparse );
+   argparse.process( app );
 
-  QMessageBox messageBox( QMessageBox::Information,
-                          title,
-                          text,
-                          QMessageBox::NoButton,
-                          nullptr,
-                          Qt::WindowStaysOnTopHint
-                          );
-  messageBox.show();
+   auto timeout = argparse.value("timeout").toInt();
+   auto period = argparse.value("period").toInt();
 
-  QTimer::singleShot( 1000 * time, &app, QApplication::quit );
+   DialogBox dialogBox(
+      argparse.value("window-title"),
+      argparse.value("message"),
+      period,
+      timeout
+      );
 
-  return app.exec();
+   return app.exec();
 }
